a03c099ea8c8a26ca8b68a4d82925c58e0371007
Thomas Deconinck <137479@supinfo.com>
2014-07-25 17:10:35 +0200

    Update team + edit zen mode

--- js/zen.js
+++ js/zen.js
@@ -1,7 +1,11 @@
 (function() {
 
-   document.getElementById('toggleZen').addEventListener("click",function(){
+  var zen = document.getElementById('toggleZen');
+
+  if (!!zen) {
+  	zen.addEventListener("click",function(){
       document.body.classList.toggle('zen');
    });
-
+  }
+  	
 })();
