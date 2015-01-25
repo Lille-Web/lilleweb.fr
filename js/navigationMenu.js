d22e2d326e8d47f77d61346f1f2a9c89f57149b1
Johnathan MEUNIER <johnathan2693@gmail.com>
2015-01-25 21:54:58 +0100

    add git articles in post

--- js/navigationMenu.js
+++ js/navigationMenu.js
@@ -1,7 +1,5 @@
 function createNavigationMenu(){
   var titles = document.querySelectorAll('article h2, article h3');
-  var bigTitles = document.querySelectorAll('article h2');
-  var subTitles = document.querySelectorAll('article h3');
   for (var i = 0; i < titles.length; i++){
     var el = document.createElement('a');
     if(titles[i].tagName == 'H2'){
